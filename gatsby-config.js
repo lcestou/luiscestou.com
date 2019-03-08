ad200257a0822e05960c08ec70152df23973225f
Luis Cestou <maisto@gmail.com>
2019-03-08 12:47:01 -0500

    adjust headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,17 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     'gatsby-plugin-offline',
-    `gatsby-plugin-netlify`, // make sure to put last in the array
+    {
+      resolve: `gatsby-plugin-netlify`, // make sure to put last in the array
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ],
 }
