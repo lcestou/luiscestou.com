baf83076514e96350f4a136760232628ea026ea7
Luis Cestou <maisto@gmail.com>
2019-03-08 11:32:42 -0500

    title for accessibility

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ const IndexPage = () => (
 						<p>Feel free to <a className="aside__contact-link" href="mailto:contact@luiscestou.com">contact me</a> for any project that you might have in mind.</p>
 						
 						<div className="aside__social-icons">
-							<a className="linkedin-icon" href="https://www.linkedin.com/in/luis-cestou/" target="_blank" rel="nofollow noopener noreferrer"><LinkedinIcon /></a>
-							<a className="email-icon" href="mailto:contact@luiscestou.com"><EmailIcon /></a>
+							<a className="linkedin-icon" href="https://www.linkedin.com/in/luis-cestou/" target="_blank" rel="nofollow noopener noreferrer" title="LinkedIn"><LinkedinIcon /></a>
+							<a className="email-icon" href="mailto:contact@luiscestou.com" title="Send Email"><EmailIcon /></a>
 						</div>
 						
 						<p className="aside__copyright">© {new Date().getFullYear()} Made with <span className="love-icon">&hearts;</span> by Luis Cestou</p>
@@ -44,7 +44,7 @@ const IndexPage = () => (
 					
 						<section className="gallery">
 
-							<AniLink cover bg="#eb8f00" to="/revive/" direction="left">
+							<AniLink cover bg="#eb8f00" to="/revive/" direction="left" title="Revive">
 								<div className="parent">
 									<div className="child bg-revive">
 										<h2 className="gallery__title">Revive</h2>
@@ -52,7 +52,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/the-stork/">
+							<AniLink cover bg="#eb8f00" to="/the-stork/" title="The Stork">
 								<div className="parent">
 									<div className="child bg-stork">
 										<h2 className="gallery__title">The Stork</h2>
@@ -60,7 +60,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 							
-							<AniLink cover bg="#eb8f00" to="/startup-weekend-pgh/">
+							<AniLink cover bg="#eb8f00" to="/startup-weekend-pgh/" title="Startup Weekend PGH">
 								<div className="parent">
 									<div className="child bg-swpgh">
 										<h2 className="gallery__title">Startup Weekend</h2>
@@ -68,7 +68,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/ovulation-predictor-calculator/">
+							<AniLink cover bg="#eb8f00" to="/ovulation-predictor-calculator/" title="Ovulation Predictor Calculator">
 								<div className="parent">
 									<div className="child bg-calc">
 										<h2 className="gallery__title">Ovulation Calc</h2>
@@ -76,7 +76,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/two-dogs-custom-woodworking/">
+							<AniLink cover bg="#eb8f00" to="/two-dogs-custom-woodworking/" title="Two Dogs Custom Woodworking">
 								<div className="parent">
 									<div className="child bg-2dogs">
 										<h2 className="gallery__title">Two Dogs</h2>
@@ -84,7 +84,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/discord-status-widget/">
+							<AniLink cover bg="#eb8f00" to="/discord-status-widget/" title="Discord Widget">
 								<div className="parent">
 									<div className="child bg-discord">
 										<h2 className="gallery__title">Discord Widget</h2>
@@ -92,7 +92,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/twenty-one-pilots/">
+							<AniLink cover bg="#eb8f00" to="/twenty-one-pilots/" title="Twenty One Pilots">
 								<div className="parent">
 									<div className="child bg-top">
 										<h2 className="gallery__title">TØP</h2>
@@ -100,7 +100,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/treatspace/">
+							<AniLink cover bg="#eb8f00" to="/treatspace/" title="Treatspace">
 								<div className="parent">
 									<div className="child bg-treatspace">
 										<h2 className="gallery__title">Treatspace</h2>
@@ -108,7 +108,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/bollinger-tech-services/">
+							<AniLink cover bg="#eb8f00" to="/bollinger-tech-services/" title="Bollinger">
 								<div className="parent">
 									<div className="child bg-bollinger">
 										<h2 className="gallery__title">Bollinger Tech</h2>
@@ -116,7 +116,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/wow-lazy-macros/">
+							<AniLink cover bg="#eb8f00" to="/wow-lazy-macros/" title="WoW Lazy Macros">
 								<div className="parent">
 									<div className="child bg-wlm">
 										<h2 className="gallery__title">WoW Lazy</h2>
@@ -124,7 +124,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/lou-ruscitto/">
+							<AniLink cover bg="#eb8f00" to="/lou-ruscitto/" title="Lou Ruscitto">
 								<div className="parent">
 									<div className="child bg-ruscitto">
 										<h2 className="gallery__title">Lou Ruscitto</h2>
@@ -132,7 +132,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/zeiss/">
+							<AniLink cover bg="#eb8f00" to="/zeiss/" title="ZEISS">
 								<div className="parent">
 									<div className="child bg-zeiss">
 										<h2 className="gallery__title">ZEISS</h2>
@@ -140,7 +140,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/quality-water-systems/">
+							<AniLink cover bg="#eb8f00" to="/quality-water-systems/" title="Quality Water Systems">
 								<div className="parent">
 									<div className="child bg-qws">
 										<h2 className="gallery__title">Quality Water</h2>
@@ -148,7 +148,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/blue-hawk/">
+							<AniLink cover bg="#eb8f00" to="/blue-hawk/" title="Blue Hawk">
 								<div className="parent">
 									<div className="child bg-bluehawk">
 										<h2 className="gallery__title">Blue Hawk</h2>
@@ -156,7 +156,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/youngstown-state-university/">
+							<AniLink cover bg="#eb8f00" to="/youngstown-state-university/" title="YSU">
 								<div className="parent">
 									<div className="child bg-ysu">
 										<h2 className="gallery__title">YSU</h2>
@@ -164,7 +164,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/general-psychology/">
+							<AniLink cover bg="#eb8f00" to="/general-psychology/" title="General Psychology">
 								<div className="parent">
 									<div className="child bg-psych">
 										<h2 className="gallery__title">General Psych</h2>
@@ -172,7 +172,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/peppers/">
+							<AniLink cover bg="#eb8f00" to="/peppers/" title="Pepper's">
 								<div className="parent">
 									<div className="child bg-peppers">
 										<h2 className="gallery__title">Pepper's</h2>
@@ -180,7 +180,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/world-wildlife-fund/">
+							<AniLink cover bg="#eb8f00" to="/world-wildlife-fund/" title="WWF">
 								<div className="parent">
 									<div className="child bg-wwf">
 										<h2 className="gallery__title">WWF</h2>
@@ -188,7 +188,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/nectar/">
+							<AniLink cover bg="#eb8f00" to="/nectar/" title="Nectar">
 								<div className="parent">
 									<div className="child bg-nectar">
 										<h2 className="gallery__title">Nectar</h2>
@@ -196,7 +196,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/widernet/">
+							<AniLink cover bg="#eb8f00" to="/widernet/" title="Widernet">
 								<div className="parent">
 									<div className="child bg-widernet">
 										<h2 className="gallery__title">Widernet</h2>
@@ -204,7 +204,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/clinical-home-practice/">
+							<AniLink cover bg="#eb8f00" to="/clinical-home-practice/" title="CHP">
 								<div className="parent">
 									<div className="child bg-chp">
 										<h2 className="gallery__title">Clinical Home</h2>
@@ -212,7 +212,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 							
-							<AniLink cover bg="#eb8f00" to="/salon-davinci/">
+							<AniLink cover bg="#eb8f00" to="/salon-davinci/" title="DaVinci">
 								<div className="parent">
 									<div className="child bg-davinci">
 										<h2 className="gallery__title">Salon DaVinci</h2>
@@ -220,7 +220,7 @@ const IndexPage = () => (
 								</div>
 							</AniLink>
 
-							<AniLink cover bg="#eb8f00" to="/juvenile-justice">
+							<AniLink cover bg="#eb8f00" to="/juvenile-justice" title="Juvenile Justice">
 								<div className="parent">
 									<div className="child bg-juvenile">
 										<h2 className="gallery__title">Juvenile Justice</h2>
