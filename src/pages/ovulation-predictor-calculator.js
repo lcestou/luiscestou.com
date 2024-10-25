27797c1a5911fd058e64c3290cbda8a82e660932
Luis Cestou <maisto@gmail.com>
2024-10-25 10:51:51 -0400

    Update ovulation-predictor-calculator.js
    Content missing when switching to Cloudflare

--- src/pages/ovulation-predictor-calculator.js
+++ src/pages/ovulation-predictor-calculator.js
@@ -7,8 +7,48 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const OvulationCalculatorPage = () => (
   <Layout>
+    <SEO title="Ovulation Predictor Calculator" />
+    
     <main className="project">
-      {/* ... rest of your component ... */}
+      <section className="project__description">
+        <h1>Ovulation Predictor Calculator</h1>
+        <p>
+          The calculator was created with the intention to replace a PHP plugin within the Stork website. This page has the most views and by using Vue.js it will reduce the load on the server while running the app in the browser.
+        </p>
+      </section>
+      
+      <section className="project__logo"><Logo /></section>
+
+      <article className="project__content">
+        <iframe 
+          height="860" 
+          style={{width: `100%`}} 
+          scrolling="no" 
+          title="Ovulation Predictor Calculator - Vue.js" 
+          src="//codepen.io/luiscestou/embed/WaaOMd/?height=265&theme-id=0&default-tab=result" 
+          frameBorder="no" 
+          allowtransparency="true" 
+          allowFullScreen={true}>
+          See the Pen <a href='https://codepen.io/luiscestou/pen/WaaOMd/'>Ovulation Predictor Calculator - Vue.js</a> by Luis Cestou
+          (<a href='https://codepen.io/luiscestou'>@luiscestou</a>) on <a href='https://codepen.io'>CodePen</a>.
+        </iframe>
+      </article>
+
+      <footer className="project__footer">
+        <div className="project__footer--gallery">
+          <AniLink cover bg="#eb8f00" direction="right" to="/startup-weekend-pgh/">
+            <div className="parent">
+              <div className="child bg-swpgh"><h2><span>&laquo;</span> Prev</h2></div>
+            </div>
+          </AniLink>
+
+          <AniLink cover bg="#eb8f00" to="/two-dogs-custom-woodworking/">
+            <div className="parent">
+              <div className="child bg-2dogs"><h2>Next <span>&raquo;</span></h2></div>
+            </div>
+          </AniLink>
+        </div>
+      </footer>
     </main>
   </Layout>
 )
